0cf59a592de38a556233fb350f3f19e75cffc01c
dcpp <dmitriy.dolinin.v@gmail.com>
2021-12-09 20:04:19 +0300

    Initialize

--- scripts/deploy-lithplusproxy.js
+++ scripts/deploy-lithplusproxy.js
@@ -5,6 +5,8 @@ async function main() {
   const proxy = await LITHplusProxy.deploy(process.env.LITHPLUS_ADDRESS, process.env.OWNER_ADDRESS, "0x");
 
   await proxy.deployed();
+  await proxy.initialize("https://ipfs.io/ipfs/", 
+    ["QmQntdqVXEf1coijvwNi3a5CiRrrg3dfAEc3fmFRUsgWyM", "QmeMVv4xmRHKu39GomAtt9HAYxQU7cmeJ5QL3fTNvEMD36"]);
 
   console.log("LITHplusProxy deployed to:", proxy.address);
 }
